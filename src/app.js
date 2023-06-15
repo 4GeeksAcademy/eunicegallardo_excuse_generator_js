d92076779ef243ec9c98c8b1f61b819d23bdcfb0
Eunice Gallardo <128846071+Eunicegallardo@users.noreply.github.com>
2023-06-15 06:49:12 +0000

    Finished Excuse Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let generateExcuse = () => {
+    let who = ["The dog", "My sister", "His turtle", "My bird"];
+    let action = ["bite", "took", "ate", "ran on"];
+    let what = ["my homework", "my smartphone", "my hat"];
+    let when = ["this morning.", "yesterday.", "before class."];
+
+    let who2 = Math.floor(Math.random() * who.length);
+    let action2 = Math.floor(Math.random() * action.length);
+    let what2 = Math.floor(Math.random() * what.length);
+    let when2 = Math.floor(Math.random() * when.length);
+    return (
+      who[who2] + " " + action[action2] + " " + what[what2] + " " + when[when2]
+    );
+  };
+  document.getElementById("excuse").innerHTML = generateExcuse();
+
+  console.log("Excuse Generator");
 };
