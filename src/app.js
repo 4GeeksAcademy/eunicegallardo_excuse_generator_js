4a4e6d6664358423eb0010bc01158cf4ab84a04c
Eunice Gallardo <128846071+Eunicegallardo@users.noreply.github.com>
2023-06-17 02:39:11 +0000

    corrected names in return

--- src/app.js
+++ src/app.js
@@ -12,12 +12,18 @@ window.onload = function() {
     let what = ["my homework", "my smartphone", "my hat"];
     let when = ["this morning.", "yesterday.", "before class."];
 
-    let who2 = Math.floor(Math.random() * who.length);
-    let action2 = Math.floor(Math.random() * action.length);
-    let what2 = Math.floor(Math.random() * what.length);
-    let when2 = Math.floor(Math.random() * when.length);
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
     return (
-      who[who2] + " " + action[action2] + " " + what[what2] + " " + when[when2]
+      who[randomWho] +
+      " " +
+      action[randomAction] +
+      " " +
+      what[randomWhat] +
+      " " +
+      when[randomWhen]
     );
   };
   document.getElementById("excuse").innerHTML = generateExcuse();
